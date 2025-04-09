1e99e86cd10b6adaf361500aaa502b97e9c37574
Davi Camilo Caetano <davi357c@gmail.com>
2025-04-09 13:39:36 -0300

    a

--- Projeto2/screens/counter.js
+++ Projeto2/screens/counter.js
@@ -1,8 +1,11 @@
-import { ScrollView, Text, Button, StyleSheet, View } from 'react-native';
+import { ScrollView, Text, Button, StyleSheet, View, TextInput } from 'react-native';
 import {useState} from 'react';
 
 export default function Counter() {
     const [contador, setContador] = useState(0);
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+
 
     function Aumentar() {
         setContador(contador + 1);
@@ -19,15 +22,31 @@ export default function Counter() {
             <Text style={styles.text}>Contador:</Text>
             <View style={{flexDirection: 'row', justifyContent: 'space-between', width: "80%"}}>
                 <View style={{flex: 1, marginHorizontal: 5, marginVertical: 20}}>
-                    <Button title="+" color="#8bf" onPress={Aumentar} />
+                    <Button title="-" color="#8bf" onPress={Diminuir} />
                 </View>
                 <View style={{flex: 1, marginHorizontal: 5, marginVertical: 20}}>
-                    <Button title="-" color="#8bf" onPress={Diminuir} style={{fontSize: 20}}/>
+                    <Button title="+" color="#8bf" onPress={Aumentar} />
                 </View>
             </View>
             <View>
                 <Text style={{fontSize: 50, margin: 30, borderWidth: 2, borderRadius: 15, paddingVertical: 5, paddingHorizontal: 20, textAlign: 'center'}}>{contador}</Text>
             </View>
+            <View>
+                <TextInput style={styles.input}
+                    placeholder="Nome"
+                    placeholderTextColor={'#469'}
+                    value={nome}
+                    onChangeText={setNome}
+                />
+                <TextInput style={styles.input}
+                    placeholder="Email"
+                    placeholderTextColor={'#469'}
+                    value={email}
+                    onChangeText={setEmail}    
+                />
+            </View>
+            <Text style={styles.text}>Nome: {nome}</Text>
+            <Text style={styles.text}>Email: {email}</Text>
         </View>
     );
 }
@@ -45,5 +64,13 @@ const styles = StyleSheet.create({
         color: '#333',
         marginBottom: 10,
         textAlign: 'center',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: '#bdf',
+        borderRadius: 10
     },
 });
